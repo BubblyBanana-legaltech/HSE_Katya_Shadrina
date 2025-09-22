c22fe3b5cfa0c2b82a40fe1bc736fd81700241c9
Екатерина Шадрина <Ek.Al.Shadrina@gmail.com>
2025-09-22 10:06:50 +0300

    dz12

---
+++ Lesson 12 (DZ_12).py
@@ -0,0 +1,24 @@
+При анализе обнаружены неофициальные API-эндпоинты, такие как:
+https://www.nasdaq.com/
+
+https://api.nasdaq.com/api/quote/{symbol}/info
+https://api.nasdaq.com/api/marketmovers
+https://api.nasdaq.com/api/calendar/earnings
+
+https://www.lme.com/
+
+https://www.lme.com/api/MarketData/...
+https://www.lme.com/api/News/...
+
+https://5ka.ru/
+https://5ka.ru/api/v2/special_offers/
+https://5ka.ru/api/v1/categories/
+https://5ka.ru/api/v1/search/suggest/
+
+https://fedresurs.ru/
+POST https://fedresurs.ru/backend/api/publication-search
+GET https://fedresurs.ru/backend/api/publication/{id}
+GET https://fedresurs.ru/backend/api/statistics/...
+
+https://kad.arbitr.ru/
+POST https://kad.arbitr.ru/Kad/SearchInstances
